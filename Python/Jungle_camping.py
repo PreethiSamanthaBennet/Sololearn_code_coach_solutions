12f763017376414ac43f99692659b918bcb2c423
Preethi Samantha Bennet <82455813+PreethiSamanthaBennet@users.noreply.github.com>
2023-01-13 00:00:42 +0530

    Create Jungle_camping.py

---
+++ Python/Jungle_camping.py
@@ -0,0 +1,14 @@
+x = input().split()
+
+for noise in x:
+	if noise == 'Grr':
+		print("Lion ", end = (''))
+	
+	if noise == 'Rawr':
+		print("Tiger ",end = (''))
+	
+	if noise == 'Ssss':
+		print("Snake ",end = (''))
+	
+	if noise == 'Chirp':
+		print("Bird ",end = (''))
