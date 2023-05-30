234efacea416b98e47f128acba35183597a816f0
Preethi Samantha Bennet <82455813+PreethiSamanthaBennet@users.noreply.github.com>
2023-05-30 17:03:22 +0530

    Create Gotham_City.c

---
+++ C/Gotham_City.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+	int x;
+  scanf("%d", &x);
+
+  if (x<5) 
+      printf("I got this!\n");
+    
+  else if (x>10) 
+      printf("Good Luck out there!\n");
+
+  else 
+      printf("Help me Batman\n");
+        
+  return 0;
+}
