bc3088401fbcb46e36f9862a94cb3bb04cd22a7c
Preethi Samantha Bennet <82455813+PreethiSamanthaBennet@users.noreply.github.com>
2023-05-30 17:23:05 +0530

    Create Gotham_City.cpp

---
+++ C++/Gotham_City.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x;
+    cin>>x;
+    
+    if(x<5){
+        cout<<"I got this!"<<endl;
+    }
+  
+    else {
+        if(x>10){
+            cout<<"Good Luck out there!"<<endl;
+        }
+        else{
+            cout<<"Help me Batman"<<endl;
+        }
+    }
+    return 0;
+}
